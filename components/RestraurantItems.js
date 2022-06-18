016c82877339bda60df4b331a4539ad6327cc8f0
Chanakyha <vetrichanakyha2003@gmail.com>
2022-06-18 22:25:14 +0530

    Making the Home Screen to get Dynamic Data

---
+++ components/RestraurantItems.js
@@ -0,0 +1,104 @@
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+export const localRestaurants = [
+  {
+    id: 1,
+    name: "Roadside Restaurant",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqI6O0wsQI5Jc6Q6Lwr7wzAUyCtQ1O5AW-jg&usqp=CAU",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1344,
+    rating: 4.5,
+  },
+  {
+    id: 1,
+    name: "Roadside Restaurant",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqI6O0wsQI5Jc6Q6Lwr7wzAUyCtQ1O5AW-jg&usqp=CAU",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1344,
+    rating: 4.5,
+  },
+  {
+    id: 1,
+    name: "Roadside Restaurant",
+    imageUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqI6O0wsQI5Jc6Q6Lwr7wzAUyCtQ1O5AW-jg&usqp=CAU",
+    categories: ["Cafe", "Bar"],
+    price: "$$",
+    reviews: 1344,
+    rating: 4.5,
+  },
+];
+
+export default function RestraurantItems({ restaurantsData }) {
+  return (
+    <TouchableOpacity activeOpacity={1} style={{ marginBottom: 30 }}>
+      {restaurantsData.map((resturant, key) => (
+        <View
+          key={key}
+          style={{ marginTop: 10, backgroundColor: "#fff", padding: 15 }}
+        >
+          <RestraurantImage image={resturant.imageUrl} />
+          <RestraurantInfo rating={resturant.rating} name={resturant.name} />
+        </View>
+      ))}
+    </TouchableOpacity>
+  );
+}
+
+const RestraurantImage = ({ image }) => (
+  <>
+    <Image
+      source={{
+        uri: image,
+      }}
+      style={{
+        width: "100%",
+        height: 180,
+      }}
+    />
+    <TouchableOpacity style={{ position: "absolute", right: 20, top: 20 }}>
+      <MaterialCommunityIcons name="heart-outline" size={25} color="#fff" />
+    </TouchableOpacity>
+  </>
+);
+
+const RestraurantInfo = ({ name, rating }) => (
+  <View
+    style={{
+      flexDirection: "row",
+      justifyContent: "space-between",
+      alignItems: "center",
+      marginTop: 10,
+    }}
+  >
+    <View>
+      <Text
+        style={{
+          fontSize: 15,
+          fontWeight: "bold",
+        }}
+      >
+        {name}
+      </Text>
+      <Text style={{ fontSize: 13, color: "gray" }}>30-45 • min</Text>
+    </View>
+    <View
+      style={{
+        backgroundColor: "#eee",
+        height: 30,
+        width: 30,
+        alignItems: "center",
+        borderRadius: 15,
+        justifyContent: "center",
+      }}
+    >
+      <Text>{rating}</Text>
+    </View>
+  </View>
+);
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,15 @@
 import { SafeAreaView, Text, View, ScrollView } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import HeaderTabs from "../components/HeaderTabs";
 import SearchBar from "../components/SearchBar";
 import Categories from "../components/Categories";
-import RestraurantItem from "../components/RestraurantItem";
+import RestraurantItems, {
+  localRestaurants,
+} from "../components/RestraurantItems";
 
 export default function Home() {
+  const [resturantData, setResturantData] = useState(localRestaurants);
+
   return (
     <SafeAreaView style={{ backgroundColor: "#eee", flex: 1 }}>
       <View style={{ backgroundColor: "#fff", padding: 15 }}>
@@ -14,7 +18,7 @@ export default function Home() {
       </View>
       <ScrollView showsVerticalScrollIndicator={0}>
         <Categories />
-        <RestraurantItem />
+        <RestraurantItems restaurantsData={resturantData} />
       </ScrollView>
     </SafeAreaView>
   );
