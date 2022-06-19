8782cd19cecc5ca3322f7ae4a38e8eb77a1036c5
Chanakyha <vetrichanakyha2003@gmail.com>
2022-06-19 16:18:43 +0530

    About Page with Static Data Food is Finished

---
+++ components/MenuItems.js
@@ -0,0 +1,92 @@
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
+import React from "react";
+import { Divider } from "react-native-elements";
+
+const foods = [
+  {
+    title: "Tandoori Chicken",
+    description:
+      "Amazing Indian Dish with tenderloin chicken off sizzling sauce of mayoo",
+    price: "$19.20",
+    image:
+      "https://media.istockphoto.com/photos/tandoori-chicken-whole-with-naan-and-raita-picture-id953514746?k=20&m=953514746&s=612x612&w=0&h=We9WAV6sG3DQaVaAESUlYlXt93yyQojSwobNuyhlXns=",
+  },
+  {
+    title: "Tandoori Chicken",
+    description:
+      "Amazing Indian Dish with tenderloin chicken off sizzling sauce of mayoo",
+    price: "$19.20",
+    image:
+      "https://media.istockphoto.com/photos/tandoori-chicken-whole-with-naan-and-raita-picture-id953514746?k=20&m=953514746&s=612x612&w=0&h=We9WAV6sG3DQaVaAESUlYlXt93yyQojSwobNuyhlXns=",
+  },
+  {
+    title: "Tandoori Chicken",
+    description:
+      "Amazing Indian Dish with tenderloin chicken off sizzling sauce of mayoo",
+    price: "$19.20",
+    image:
+      "https://media.istockphoto.com/photos/tandoori-chicken-whole-with-naan-and-raita-picture-id953514746?k=20&m=953514746&s=612x612&w=0&h=We9WAV6sG3DQaVaAESUlYlXt93yyQojSwobNuyhlXns=",
+  },
+  {
+    title: "Tandoori Chicken",
+    description:
+      "Amazing Indian Dish with tenderloin chicken off sizzling sauce of mayoo",
+    price: "$19.20",
+    image:
+      "https://media.istockphoto.com/photos/tandoori-chicken-whole-with-naan-and-raita-picture-id953514746?k=20&m=953514746&s=612x612&w=0&h=We9WAV6sG3DQaVaAESUlYlXt93yyQojSwobNuyhlXns=",
+  },
+  {
+    title: "Tandoori Chicken",
+    description:
+      "Amazing Indian Dish with tenderloin chicken off sizzling sauce of mayoo",
+    price: "$19.20",
+    image:
+      "https://media.istockphoto.com/photos/tandoori-chicken-whole-with-naan-and-raita-picture-id953514746?k=20&m=953514746&s=612x612&w=0&h=We9WAV6sG3DQaVaAESUlYlXt93yyQojSwobNuyhlXns=",
+  },
+];
+
+const styles = StyleSheet.create({
+  menuItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 20,
+  },
+
+  titleStyle: {
+    fontSize: 19,
+    fontWeight: "600",
+  },
+});
+
+export default function MenuItems() {
+  return (
+    <ScrollView showsVerticalScrollIndicator={0}>
+      {foods.map((food, key) => (
+        <View key={key}>
+          <View style={styles.menuItem}>
+            <FoodInfo food={foods[0]} />
+            <FoodImage food={foods[0]} />
+          </View>
+          <Divider width={0.5} orientation={"vertical"} />
+        </View>
+      ))}
+    </ScrollView>
+  );
+}
+
+const FoodInfo = ({ food }) => (
+  <View style={{ width: 240, justifyContent: "space-evenly" }}>
+    <Text style={styles.titleStyle}>{food.title}</Text>
+    <Text>{food.description}</Text>
+    <Text>{food.price}</Text>
+  </View>
+);
+
+const FoodImage = ({ food }) => (
+  <View>
+    <Image
+      source={{ uri: food.image }}
+      style={{ width: 100, height: 100, borderRadius: 8 }}
+    />
+  </View>
+);
--- screens/RestaurantDetail.js
+++ screens/RestaurantDetail.js
@@ -2,12 +2,14 @@ import { View, Text } from "react-native";
 import React from "react";
 import { Divider } from "react-native-elements";
 import About from "../components/About";
+import MenuItems from "../components/MenuItems";
 
 export default function RestaurantDetail() {
   return (
     <View>
       <About />
       <Divider width={1.8} style={{ marginVertical: 20 }} />
+      <MenuItems />
     </View>
   );
 }
