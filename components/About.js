ca5e30542a31c5e2547c925b09c64960b189a389
Chanakyha <vetrichanakyha2003@gmail.com>
2022-06-19 21:22:17 +0530

    Made the Static Data to compatible with Dynamic Data

--- components/About.js
+++ components/About.js
@@ -1,15 +1,28 @@
 import { View, Text, Image } from "react-native";
 import React from "react";
 
-const image =
-  "https://www.madridcoolblog.com/wp-content/uploads/2016/08/MADRID-COOL-BLOG-RANDOM-entrada-G-800x534.jpg";
-const title = "Farmhouse Kitchen Thai Cusine";
-const description = "Thai • Comfort Food • $$ • 🎫 • 4 ⭐ • (2913+) ";
+const yelpRestaurantInfo = {
+  name: "Farmhouse Kitchen Thai Cusine",
+  image:
+    "https://www.madridcoolblog.com/wp-content/uploads/2016/08/MADRID-COOL-BLOG-RANDOM-entrada-G-800x534.jpg",
+  price: "$$",
+  reviews: "1500",
+  rating: 4.5,
+  catagories: [{ title: "Thai" }, { title: "Comfort Food" }],
+};
+
+const { name, image, catagories, price, reviews, rating } = yelpRestaurantInfo;
+const formattedCatagories = catagories.map((cat) => cat.title).join(" • ");
+
+const description = `${formattedCatagories} ${
+  price ? ` • ${price}` : ""
+} • 🎫 • ${rating} 🌟(${reviews}+)`;
+
 export default function About() {
   return (
     <View>
       <RestaurantImage image={image} />
-      <RestaurantTitle title={title} />
+      <RestaurantTitle name={name} />
       <RestaurantDescription description={description} />
     </View>
   );
@@ -19,7 +32,7 @@ const RestaurantImage = ({ image }) => (
   <Image source={{ uri: image }} style={{ width: "100%", height: 180 }} />
 );
 
-const RestaurantTitle = ({ title }) => (
+const RestaurantTitle = ({ name }) => (
   <Text
     style={{
       fontSize: 29,
@@ -28,7 +41,7 @@ const RestaurantTitle = ({ title }) => (
       marginHorizontal: 15,
     }}
   >
-    {title}
+    {name}
   </Text>
 );
 
